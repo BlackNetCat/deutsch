98f2c9cb4d3d1a952e1ace43aa02cdfb5f87640d
netcat <melnyk.alexandr@gmail.com>
2019-12-31 12:14:42 +0200

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Store from '../store/'
 import Home from '../views/Home.vue'
+import Books from '@/views/Books.vue'
+import Words from '@/views/Words.vue'
+import Signin from '@/views/Signin.vue'
+import Signup from '@/views/Signup.vue'
+import Profile from '@/views/Profile.vue'
+import Logout from '@/views/Logout.vue'
 
 Vue.use(VueRouter)
 
@@ -11,13 +18,37 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/books',
+    name: 'books',
+    component: Books
+  },
+  {
+    path: '/words',
+    name: 'words',
+    component: Words
+  },
+  {
+    path: '/signin',
+    name: 'signin',
+    component: Signin
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: Signup
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: Profile,
+    beforeEnter: AuthGuard
+  },
+  {
+    path: '/logout',
+    name: 'logout',
+    component: Logout
+  },
+
 ]
 
 const router = new VueRouter({
@@ -27,3 +58,10 @@ const router = new VueRouter({
 })
 
 export default router
+
+function AuthGuard (from, to, next) {
+  if(Store.getters.isUserAuthenticated)
+      next()
+  else
+    next('/signin')
+}
